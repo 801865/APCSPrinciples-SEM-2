95021126c219994a00af2aec46539862f693e461
iMac <imac@imacs-imac.attlocal.net>
2020-01-29 15:15:28 -0800

    art con.

--- Lab 128 ArtOne/boid.js
+++ Lab 128 ArtOne/boid.js
@@ -12,4 +12,39 @@ class Boid{
     this.update();
     this.render();
   }
+
+  checkEdges(){
+    if(this.loc.x < 0){
+      this.acc.mult(0);
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.x > width){
+      this.acc.mult(0);
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.y < 0){
+      this.acc.mult(0);
+      this.vel.y = -this.vel.y;
+    }
+    if(this.loc.y > height){
+      this.acc.mult(0);
+      this.vel.y = -this.vel.y;
+    }
+  }
+
+  update(){
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+    this.vel.limit(2);
+  }
+
+  render(){
+    var distToBoid = 0;
+    for(var j = 0; j < boids.length - 1; j++){
+      distToBoid = boid[j].loc.dist(boid[j + 1].loc);
+      if(distToBoid < 100){
+        line(boid[j].x, boid[j].y, boid[j + 1].x, boid[j+1].y)
+      }
+    }
+  }
 }
--- Lab 128 ArtOne/sketch.js
+++ Lab 128 ArtOne/sketch.js
@@ -3,21 +3,28 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 boids = [];
+var num = 100;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadBoids(100);
+  loadBoids(num);
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-
+  runBoids(num);
 }
 
 function loadBoids(n){
   for(var i = 0; i < n; i++){
       boids[i] = new Boid(random(width), random(height), random(-10, 10), random(-10, 10));
+  }
+}
+
+function runBoids(n){
+  for(var i = 0; i < n; i++){
+      boids[i].run();
   }
 }
